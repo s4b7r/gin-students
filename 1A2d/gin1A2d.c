9e0b339e5c1d55a153e9dc19fb0d58213655fbd8
Simon Bekemeier <simon.bekemeier@fh-bielefeld.de>
2020-10-24 17:29:14 +0200

    Extract function as suggested by student

--- 1A2d/gin1A2d.c
+++ 1A2d/gin1A2d.c
@@ -1,9 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+float Zahl1, Zahl2;
+
+void festlegenGanzzahligerWerte() {
+    printf("Legen Sie Ihre ganzzahligen Werte fest:\n\n");
+    printf("a = ");
+    scanf("%f", &Zahl1);
+    printf("b = ");
+    scanf("%f", &Zahl2);
+}
+
 int main(int argc, char** argv) {
-    float Zahl1;
-    float Zahl2;
     float Ergebnis;
     int Auswahl;
 
@@ -14,46 +22,22 @@ int main(int argc, char** argv) {
     printf("_______________________________________________________________\n");
 
     if (Auswahl == 1) { //Addition
-        printf("Legen Sie Ihre ganzzahligen Werte fest:\n\n");
-        printf("a = ");
-        scanf("%f", &Zahl1);
-
-        printf("b = ");
-        scanf("%f", &Zahl2);
-
+        festlegenGanzzahligerWerte();
         Ergebnis = Zahl1 + Zahl2;
         printf("Ergebnis = %f", Ergebnis);
     }
     else if (Auswahl == 2) { //Subtraktion
-        printf("Legen Sie Ihre ganzzahligen Werte fest:\n\n");
-        printf("a = ");
-        scanf("%f", &Zahl1);
-
-        printf("b = ");
-        scanf("%f", &Zahl2);
-
+        festlegenGanzzahligerWerte();
         Ergebnis = Zahl1 - Zahl2;
         printf("Ergebnis = %f", Ergebnis);
     }
     else if (Auswahl == 3) { //Multiplikation
-        printf("Legen Sie Ihre ganzzahligen Werte fest:\n\n");
-        printf("a = ");
-        scanf("%f", &Zahl1);
-
-        printf("b = ");
-        scanf("%f", &Zahl2);
-
+        festlegenGanzzahligerWerte(); 
         Ergebnis = Zahl1 * Zahl2;
         printf("Ergebnis = %f", Ergebnis);
     }
     else if (Auswahl == 4) { //Division
-        printf("Legen Sie Ihre ganzzahligen Werte fest:\n\n");
-        printf("a = ");
-        scanf("%f", &Zahl1);
-
-        printf("b = ");
-        scanf("%f", &Zahl2);
-
+        festlegenGanzzahligerWerte();
         Ergebnis = Zahl1 / Zahl2;
         printf("Ergebnis = %f", Ergebnis);
     }
